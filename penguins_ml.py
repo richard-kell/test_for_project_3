bd7cd6cd0c84813e746750b1e018b16a5d7d8417
Richard Kell <rrkell1214@gmail.com>
2022-08-20 12:15:03 -0500

    first commit

---
+++ penguins_ml.py
@@ -0,0 +1,31 @@
+from copyreg import pickle
+from turtle import pen
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+penguine_df = pd.read_csv('penguins.csv')
+penguine_df.dropna(inplace=True)
+output = penguine_df['species']
+features = penguine_df[['island', 'bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex']]
+features = pd.get_dummies(features)
+output, uniques = pd.factorize(output)
+
+x_train, x_test, y_train, y_test = train_test_split(features, output, test_size=.8)
+rfc = RandomForestClassifier(random_state=15)
+rfc.fit(x_train, y_train)
+y_pred = rfc.predict(x_test)
+score = accuracy_score(y_pred, y_test)
+print('Accuracy score for this model is {}'.format(score))
+
+print(uniques)
+print(output)
+
+rf_pickle = open('random_forest_penguin.pickle', 'wb')
+pickle.dump(rfc, rf_pickle)
+rf_pickle.close()
+output_pickle = open('output_penguin.pickle', 'wb')
+pickle.dump(uniques, output_pickle)
+output_pickle.close()
