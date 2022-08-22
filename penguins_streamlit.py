f2eed6c480bd5ff185c3a3a7f5d4bf8cf2a53b07
Richard Kell <rrkell1214@gmail.com>
2022-08-22 06:22:21 -0500

    Fourth commit

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -20,4 +20,22 @@ bill_depth = st.number_input('Bill Depth', min_value=0)
 flipper_length = st.number_input('Flipper Length (mm)', min_value=0)
 body_mass = st.number_input('Body Mass (g)', min_value=0)
 
+island_biscoe, island_dream, island_torgerson = 0, 0, 0
+if island == 'Biscoe':
+    island_biscoe = 1
+elif island == 'Dream':
+    island_dream = 1
+elif island_torgerson == 'Torgerson':
+    island_torgerson = 1
+
+sex_feamle, sex_male = 0, 0
+if sex == 'Female':
+    sex_female == 1
+elif sex == 'Male':
+    sex_male = 1
+
+new_prediction = rfc.predict([[bill_length, bill_depth, flipper_length, body_mass, island_biscoe, island_dream, island_torgerson, sex_feamle, sex_male]])
+prediction_species = unique_penguin_mapping[new_prediction][0]
+st.write('We predict your penguin is of the {} species'.format(prediction_species))
+
 st.write('the user inputs are {}'.format([island, sex, bill_length, bill_depth, flipper_length, body_mass]))
