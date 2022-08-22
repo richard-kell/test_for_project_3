94469b60e80dcaceafb738ee8e7e8b04db8e23f3
Richard Kell <rrkell1214@gmail.com>
2022-08-22 06:01:56 -0500

    Second commit

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -8,3 +8,12 @@ unique_penguin_mapping = pickle.load(map_pickle)
 
 st.write(rfc)
 st.write(unique_penguin_mapping)
+
+island = st.selectbox('Penguin Island', options=['Biscoe', 'Dream', 'Torgerson'])
+sex = st.selectbox('Sex', options=['Female', 'Male'])
+bill_length = st.number_input('Bill Length (mm)', min_value=0)
+bill_depth = st.number_input('Bill Depth', min_value=0)
+flipper_length = st.number_input('Flipper Length (mm)', min_value=0)
+body_mass = st.number_input('Body Mass (g)', min_value=0)
+
+st.write('the user inputs are {}'.format(island, sex, bill_length, bill_depth, flipper_length, body_mass))
